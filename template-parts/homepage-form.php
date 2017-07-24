2b909be1954ad13ca99af690f551883fd57f2d72
Leon Magee <leonmagee@hotmail.com>
2017-07-24 11:35:07 -0700

    update

--- template-parts/homepage-form.php
+++ template-parts/homepage-form.php
@@ -1,25 +1,21 @@
 <div class="search-form-wrap">
-    <div class="input-wrap one_fourth">
-
-        <button class="button" type="button" data-toggle="example-dropdown-1">All Status</button>
+    <div class="input-wrap one_fifth">
+        <div class="select-toggle" data-toggle="example-dropdown-1">All Status</div>
+        <i class="fa fa-sort" aria-hidden="true"></i>
         <div class="dropdown-pane" id="example-dropdown-1" data-dropdown data-hover="true" data-hover-pane="true">
         <ul>
             <li>For Sale</li>
-            <li>For Rent</li>
-            <li>Sold</li>
+            <li>For Lease</li>
+            <li>Sold Listings</li>
         </ul>
         </div>
-
-
-<!--        <select>-->
-<!--            <option>All Status</option>-->
-<!--            <option>For Sale</option>-->
-<!--            <option>For Rent</option>-->
-<!--            <option>Sold</option>-->
-<!--        </select>-->
     </div>
-    <div class="input-wrap three_fourths">
+    <div class="input-wrap three_fifths">
         <i class="fa fa-search" aria-hidden="true"></i>
         <input type="text" placeholder="Search City, State, or Area"/>
+    </div>
+
+    <div class="input-wrap one_fifth submit-input">
+        Submit
     </div>
 </div>
