1c87b1bcade8d19ad513eca0ac9274d075610c68
Leon Magee <leonmagee@hotmail.com>
2017-07-25 09:13:38 -0700

    update

--- template-parts/homepage-form.php
+++ template-parts/homepage-form.php
@@ -1,8 +1,8 @@
 <div class="search-form-wrap">
-    <div class="input-wrap one_fifth">
-        <div class="select-toggle" data-toggle="example-dropdown-1">All Status</div>
+    <div class="input-wrap status" data-toggle="status-dropdown">
+        <div class="select-toggle">All Status</div>
         <i class="fa fa-sort" aria-hidden="true"></i>
-        <div class="dropdown-pane" id="example-dropdown-1" data-dropdown data-hover="true" data-hover-pane="true">
+        <div class="dropdown-pane" id="status-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
         <ul>
             <li>For Sale</li>
             <li>For Lease</li>
@@ -10,12 +10,28 @@
         </ul>
         </div>
     </div>
-    <div class="input-wrap three_fifths">
+    <div class="input-wrap property-type" data-toggle="property-type-dropdown">
+        <div class="select-toggle">All Property Types</div>
+        <i class="fa fa-sort" aria-hidden="true"></i>
+        <div class="dropdown-pane" id="property-type-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
+            <ul>
+                <li>Multifamily</li>
+                <li>Office</li>
+                <li>Retail</li>
+                <li>Industrial</li>
+                <li>Hotel / Motel</li>
+                <li>Land</li>
+                <li>Residential Income</li>
+                <li>Business Opportunity</li>
+            </ul>
+        </div>
+    </div>
+    <div class="input-wrap main-input">
         <i class="fa fa-search" aria-hidden="true"></i>
         <input type="text" placeholder="Search City, State, or Area"/>
     </div>
 
-    <div class="input-wrap one_fifth submit-input">
+    <div class="input-wrap submit-input">
         Submit
     </div>
 </div>
