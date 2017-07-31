88e4fdef1bfad4c86b716cbb22989558b35e1ffe
Leon Magee <leonmagee@hotmail.com>
2017-07-31 14:47:10 -0700

    update

--- old/page-homepage-template.php
+++ old/page-homepage-template.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Template Name: Homepage
- *
  * @package MarketPier
  */
 
@@ -29,7 +27,15 @@ if ( $background = get_field( 'homepage_background_image', 'option' ) ) {
             </div>
         </div>
 
-        <div class="homepage-section homepage-section-cta">
+
+        <div class="homepage-section homepage-section-1">
+
+			<?php if ( $section_tagline_1 = get_field( 'section_tagline_1', 'option' ) ) { ?>
+                <h2 class="section-title"><?php echo $section_tagline_1; ?></h2>
+			<?php } ?>
+        </div>
+
+        <div class="homepage-section homepage-section-2">
 
             <div class="homepage-cta-boxes-outer">
 				<?php
@@ -40,11 +46,10 @@ if ( $background = get_field( 'homepage_background_image', 'option' ) ) {
                             <div class="homepage-cta-boxes-inner">
                                 <div class="cta-inside">
                                     <div class="title-wrap">
-                                        <img src="<?php echo $cta_box['icon']; ?>"/>
+                                        <img src="<?php echo $cta_box['icon']; ?>" />
                                         <h3><?php echo $cta_box['title']; ?></h3>
                                     </div>
                                     <p><?php echo $cta_box['excerpt']; ?></p>
-                                    <a class='cta-link' href="<?php echo site_url()  . '/' . $cta_box['url']; ?>"><?php echo $cta_box['link_text']; ?></a>
                                 </div>
                             </div>
 						<?php } ?>
@@ -54,12 +59,30 @@ if ( $background = get_field( 'homepage_background_image', 'option' ) ) {
 
         </div>
 
-        <div class="homepage-section homepage-section-tagline">
 
-			<?php if ( $section_tagline_1 = get_field( 'section_tagline_1', 'option' ) ) { ?>
-                <h2 class="section-title"><?php echo $section_tagline_1; ?></h2>
-			<?php } ?>
+        <div class="homepage-section homepage-section-3">
+
+            <div class="homepage-cta-boxes-outer">
+			    <?php
+			    $homepage_cta_boxes = get_field( 'homepage_cta_boxes', 'option' );
+			    if ( $homepage_cta_boxes ) { ?>
+                    <div class="homepage-cta-boxes-wrap">
+					    <?php foreach ( $homepage_cta_boxes as $cta_box ) { ?>
+                            <div class="homepage-cta-boxes-inner"
+                                 style="background-image: url(<?php echo $cta_box['image']; ?>)">
+                                <div class="cta-inside">
+                                    <h3><?php echo $cta_box['title']; ?></h3>
+                                    <p><?php echo $cta_box['excerpt']; ?></p>
+                                </div>
+                            </div>
+					    <?php } ?>
+                    </div>
+			    <?php } ?>
+            </div>
+
         </div>
+
+
 
 
     </div>
--- template-parts/homepage-form.php
+++ template-parts/homepage-form.php
@@ -1,10 +1,9 @@
 <div class="search-form-wrap">
     <div class="input-wrap status" data-toggle="status-dropdown">
-        <div class="select-toggle">All Status</div>
+        <div class="select-toggle">For Sale</div>
         <i class="fa fa-sort" aria-hidden="true"></i>
         <div class="dropdown-pane" id="status-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
         <ul>
-            <li>For Sale</li>
             <li>For Lease</li>
             <li>Sold Listings</li>
         </ul>
