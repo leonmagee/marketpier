5f9e18dc1a63739d3d92ef8ad8a8b7512580d80c
Leon Magee <leonmagee@hotmail.com>
2017-08-01 18:33:40 -0700

    update

--- inc/lv_google_map_group.php
+++ inc/lv_google_map_group.php
@@ -45,8 +45,10 @@ class lv_google_map_group {
                         lng: <?php echo $longitude; ?>
                     },
                     address: '<?php echo $location['address']; ?>',
+                    price: '<?php echo $location['price']; ?>',
                     url: '<?php echo $location['url']; ?>'
                 });
+                console.log(latlngArray);
 
 				<?php } ?>
 
@@ -55,11 +57,12 @@ class lv_google_map_group {
                 var latitude_array = [];
                 var longitude_array = [];
                 var url_array = [];
+                var price_array = [];
                 var geo = new google.maps.Geocoder();
 
                 getLatLngAddress(latlngArray, len, geo);
 
-                function getLatLngAddress(latlngArray, array_length, geo) {
+                function getLatLngAddress(latlngArray, array_length, geo, listing_price = null) {
 
                     if (count < array_length) {
 
@@ -67,6 +70,7 @@ class lv_google_map_group {
                         var latitude = latlngArray[count].latLng.lat;
                         var longitude = latlngArray[count].latLng.lng;
                         var listing_url = latlngArray[count].url;
+                        var listing_price = latlngArray[count].price;
 
                         geo.geocode({address: new_address}, function (results, status) {
 
@@ -82,6 +86,7 @@ class lv_google_map_group {
                             latitude_array.push(latitude);
                             longitude_array.push(longitude);
                             url_array.push(listing_url);
+                            price_array.push(listing_price);
 
                             ++count;
 
@@ -117,7 +122,6 @@ class lv_google_map_group {
                          */
 
                         var squareBg = {
-                            //path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
                             path: 'M95.62,34.988c0,5.522-4.478,10-10,10H14.38c-5.523,0-10-4.478-10-10V15.011c0-5.522,4.477-9.999,10-9.999h71.24c5.522,0,10,4.477,10,9.999V34.988z',
                             fillColor: '#00A3E4',
                             fillOpacity: 1,
@@ -125,32 +129,26 @@ class lv_google_map_group {
                             strokeColor: '#FFF',
                             strokeWeight: 3,
                             color: '#FFF',
-                            labelOrigin: new google.maps.Point(47, 20),
+                            labelOrigin: new google.maps.Point(47, 25),
                             anchor: new google.maps.Point(9, 35),
                         };
 
-                        var icon_image = 'http://www.therealtyfirm.com/wp-content/uploads/map-marker.png';
-
                         for (var index = 0; index < array_length; ++index) {
 
+                            var current_price = price_array[index];
 
+                            //var listing_price = '999k';
                             var marker = new google.maps.Marker({
                                 //position: map.getCenter(),
                                 position: {lat: latitude_array[index], lng: longitude_array[index]},
                                 //icon: goldStar,
                                 icon: squareBg,
-                                label: {text: 'yyy', color: '#FFF'},
+                                label: {text: current_price, color: '#FFF', fontWeight: '700', fontFamily: 'Open Sans', fontSize: '11px'},
                                 map: map
                             });
 
-
-//                            marker = new google.maps.Marker({
-//                                position: {lat: latitude_array[index], lng: longitude_array[index]},
-//                                map: map,
-//                                title: latlngArray[index].address
-//                            });
-
                             var current_link = url_array[index];
+
 
                             new_marker_link(marker, current_link);
 
--- inc/snippet_data_search.php
+++ inc/snippet_data_search.php
@@ -26,10 +26,29 @@ class snippet_data_search {
 
 			$snippet_objects[] = $listing_data;
 
+			$price_label = $listing_data->price;
+
+			if ( $price_label > 999999 ) {
+				$decimal = substr($price_label, -6,1);
+				$price_label = substr($price_label, 0,-6);
+				if ( $decimal ) {
+					$price_label = $price_label . '.' . $decimal . 'm';
+				} else {
+					$price_label = $price_label . 'm';
+				}
+			} elseif ( $price_label > 999 ) {
+				$price_label = substr($price_label, 0,-3);
+				$price_label = $price_label . 'k';
+			} else {
+				$price_label = '$' . $price_label;
+			}
+
 			$map_data_array_src[] = array(
 				'lat'     => $listing_data->lat,
 				'long'    => $listing_data->long,
-				'address' => $listing_data->combined_address
+				'address' => $listing_data->combined_address,
+				'price'   => $price_label,
+				'url'     => $listing_data->listing_url
 			);
 
 		}
