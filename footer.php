c808aba534d5088d81803c6a69fbdc068955652d
Leon Magee <leonmagee@hotmail.com>
2017-08-03 10:24:05 -0700

    update

--- footer.php
+++ footer.php
@@ -16,22 +16,38 @@
 <footer id="colophon" class="site-footer">
     <div class="site-footer-inner">
         <div class="attribution">
-            <div class="attribution-line">
-                <a href="<?php echo site_url(); ?>/about">About</a>
+            <div class="attribution-top">
+                <div class="attribution-line">
+					<?php echo bloginfo( 'site_title' ); ?> <?php echo date( 'Y' ); ?> All Rights Reserved
+                </div>
             </div>
-            <span class="sep"> | </span>
-            <div class="attribution-line">
-                <a href="<?php echo site_url(); ?>/contact">Contact</a>
-            </div>
-            <span class="sep"> | </span>
-            <div class="attribution-line">
-				<?php echo bloginfo( 'site_title' ); ?> <?php echo date( 'Y' ); ?> All Rights Reserved
-            </div>
-            <span class="sep"> | </span>
-            <div class="attribution-line">
-                Site by <a href="https://levon.io" target="_blank">Levon.io</a>
+            <div class="attribution-bottom">
+                <div class="attribution-line">
+                    <a href="<?php echo site_url(); ?>/about">About</a>
+                </div>
+                <span class="sep"> | </span>
+                <div class="attribution-line">
+                    <a href="<?php echo site_url(); ?>/contact">Contact</a>
+                </div>
+                <span class="sep"> | </span>
+                <div class="attribution-line">
+                    Site by <a href="https://levon.io" target="_blank">Levon.io</a>
+                </div>
             </div>
         </div>
+
+		<?php if ( $broker_agent_info = get_field( 'broker_agent_info', 'option' ) ) { ?>
+            <div class="broker-info-line">
+				<?php echo $broker_agent_info; ?>
+            </div>
+		<?php } ?>
+
+	    <?php if ( $broker_company_info = get_field( 'broker_company_info', 'option' ) ) { ?>
+            <div class="broker-info-line">
+			    <?php echo $broker_company_info; ?>
+            </div>
+	    <?php } ?>
+
 
         <div class="social">
 			<?php get_template_part( 'template-parts/social-media' ); ?>
