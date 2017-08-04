07de12e9a42f5fd24df0159ad06755c4457808e9
Leon Magee <leonmagee@hotmail.com>
2017-08-04 08:56:25 -0700

    update

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,28 @@
      * Initialize Foundation
      */
     $(document).foundation();
+
+
+    /**
+     * Handle Listing Search Functionality
+     * The search bar uses foundation dropdowns to simulate a normal-ish search form.
+     * @todo test on mobile!
+     */
+
+    $('.search-form-wrap .dropdown-pane ul li').click(function () {
+        var selected_value = $(this).html();
+        //$(this).parent().parent().parent().find('.select-toggle').html(selected_value).parent().addClass('selected');
+        //$(this).parent().parent().css({'visibility': 'hidden'}).parent().find('.select-toggle').html(selected_value);
+        //@todo would be good to hide the panel on click but then it doesn't show up again on hover?
+        $(this).parent().find('.selected').removeClass('selected');
+        $(this).addClass('selected').parent().parent().parent().find('.select-toggle').html(selected_value).parent().find('input.hidden-input').val(selected_value);
+
+        // @todo set value of hidden input
+
+        //$(this).parent().find('.select-toggle').val('xxx');
+    })
+
+
 }(jQuery));
 
 
--- template-parts/homepage-form.php
+++ template-parts/homepage-form.php
@@ -1,36 +1,42 @@
 <div class="search-form-wrap">
-    <div class="input-wrap status" data-toggle="status-dropdown">
-        <div class="select-toggle">For Sale</div>
-        <i class="fa fa-sort" aria-hidden="true"></i>
-        <div class="dropdown-pane" id="status-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
-        <ul>
-            <li>For Lease</li>
-            <li>Sold Listings</li>
-        </ul>
+    <form method="post" action="#"><!-- @todo form action to switch page to search results? -->
+        <div class="input-wrap status" data-toggle="status-dropdown">
+            <div class="select-toggle">For Sale</div>
+            <i class="fa fa-sort" aria-hidden="true"></i>
+            <div class="dropdown-pane" id="status-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
+                <ul>
+                    <li>For Sale</li>
+                    <li>For Lease</li>
+                    <li>Sold Listings</li>
+                </ul>
+            </div>
+            <input class="hidden-input" type="hidden" name="status" />
         </div>
-    </div>
-    <div class="input-wrap property-type" data-toggle="property-type-dropdown">
-        <div class="select-toggle">All Property Types</div>
-        <i class="fa fa-sort" aria-hidden="true"></i>
-        <div class="dropdown-pane" id="property-type-dropdown" data-dropdown data-hover="true" data-hover-pane="true">
-            <ul>
-                <li>Multifamily</li>
-                <li>Office</li>
-                <li>Retail</li>
-                <li>Industrial</li>
-                <li>Hotel / Motel</li>
-                <li>Land</li>
-                <li>Residential Income</li>
-                <li>Business Opportunity</li>
-            </ul>
+        <div class="input-wrap property-type" data-toggle="property-type-dropdown">
+            <div class="select-toggle">All Property Types</div>
+            <i class="fa fa-sort" aria-hidden="true"></i>
+            <div class="dropdown-pane" id="property-type-dropdown" data-dropdown data-hover="true"
+                 data-hover-pane="true">
+                <ul>
+                    <li>Multifamily</li>
+                    <li>Office</li>
+                    <li>Retail</li>
+                    <li>Industrial</li>
+                    <li>Hotel / Motel</li>
+                    <li>Land</li>
+                    <li>Residential Income</li>
+                    <li>Business Opportunity</li>
+                </ul>
+            </div>
+            <input class="hidden-input" type="hidden" name="property-type" />
         </div>
-    </div>
-    <div class="input-wrap main-input">
-        <input type="text" placeholder="Search City, State, or Area"/>
-        <i class="fa fa-search" aria-hidden="true"></i>
-    </div>
+        <div class="input-wrap main-input">
+            <input type="text" placeholder="Search City, State, or Area" name="city-state-area"/>
+            <i class="fa fa-search" aria-hidden="true"></i>
+        </div>
 
-    <div class="input-wrap submit-input">
-        Submit
-    </div>
+        <div class="input-wrap submit-input">
+            Submit
+        </div>
+    </form>
 </div>
