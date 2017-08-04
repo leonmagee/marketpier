8d4f5c7d4d14fb4168f704da6c5ceb7c6a508cbb
Leon Magee <leonmagee@hotmail.com>
2017-08-03 21:28:33 -0700

    update

--- page.php
+++ page.php
@@ -2,18 +2,10 @@
 /**
  * The template for displaying all pages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
  * @package MarketPier
  */
 
 get_header(); ?>
-
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
             <div class="page-content-wrap">
@@ -25,7 +17,4 @@ get_header(); ?>
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
-
-<?php
-//get_sidebar();
-get_footer();
+<?php get_footer();
