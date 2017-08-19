4e96e423483b476f43654e02d406c29c84eefbf7
Leon Magee <leonmagee@hotmail.com>
2017-08-19 10:59:01 -0700

    update

--- template-parts/logged-in-user-sidebar.php
+++ template-parts/logged-in-user-sidebar.php
@@ -3,30 +3,33 @@ $user = wp_get_current_user();
 $username = $user->user_login;
 ?>
 <div class="logged-in-user-sidebar-wrap">
-
     <a href="<?php echo site_url(); ?>/your-profile" class="sidebar-option update-profile">
-        <span>Update Your Profile</span>
-        <i class="fa fa-chevron-right"></i>
+        <i class="fa fa-cog" aria-hidden="true"></i>
+        <span>Account Settings</span>
+    </a>
+    <a href="<?php echo site_url(); ?>/your-profile" class="sidebar-option update-profile">
+        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+        <span>Update Profile</span>
     </a>
     <a href="<?php echo site_url(); ?>/author/<?php echo $username; ?>" class="sidebar-option view-profile">
+        <i class="fa fa-user"></i>
         <span>View Your Profile</span>
-        <i class="fa fa-chevron-right"></i>
     </a>
     <a href="<?php echo site_url(); ?>/add-listing" class="sidebar-option add-listing">
+        <i class="fa fa-plus-square" aria-hidden="true"></i>
         <span>Add a Listing</span>
-        <i class="fa fa-chevron-right"></i>
     </a>
     <a href="<?php echo site_url(); ?>/your-listings" class="sidebar-option your-listings">
+        <i class="fa fa-list" aria-hidden="true"></i>
         <span>Your Listings</span>
-        <i class="fa fa-chevron-right"></i>
     </a>
     <a href="<?php echo site_url(); ?>" class="sidebar-option">
+        <i class="fa fa-home" aria-hidden="true"></i>
         <span>MarketPier Home</span>
-        <i class="fa fa-chevron-right"></i>
     </a>
     <a href="<?php echo wp_logout_url( site_url() ); ?>" class="sidebar-option">
+        <i class="fa fa-sign-out" aria-hidden="true"></i>
         <span>Log Out</span>
-        <i class="fa fa-chevron-right"></i>
     </a>
 
 </div>
