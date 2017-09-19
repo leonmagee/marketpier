9de7855591bccff364f79e825bb0de9bc1792912
Leon Magee <leonmagee@hotmail.com>
2017-09-19 15:38:36 -0700

    update

--- page-edit-listing.php
+++ page-edit-listing.php
@@ -51,59 +51,60 @@ get_template_part( 'template-parts/spinner' );
 get_header(); ?>
 
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main">
-        <div class="page-content-wrap">
-            <header class="entry-header">
-                <h1 class="entry-title">Edit Listing</h1>
-            </header>
-            <div class="logged-in-outer-wrap">
-                <div class="logged-in-user-content logged-in-edit-listing add-or-edit-listing">
-                    <div class="view-listing-link">
-                        <a href="<?php echo $permalink; ?>">View Listing</a>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <div class="page-content-wrap">
+                <header class="entry-header">
+                    <h1 class="entry-title">Edit Listing</h1>
+                </header>
+                <div class="logged-in-outer-wrap">
+                    <div class="logged-in-user-content logged-in-edit-listing add-or-edit-listing">
+                        <div class="view-listing-link">
+                            <a href="<?php echo $permalink; ?>">View Listing</a>
+                        </div>
+						<?php acf_form( array(
+							'post_id'      => $post_id,
+							'post_title'   => false,
+							'new_post'     => array(
+								'post_type'   => 'mp-listing',
+								'post_status' => 'publish'
+							),
+							//'uploader'     => 'basic',
+							'fields'       => array(
+								'listing_mls_number',
+								'listing_price',
+								'listing_monthly_rent',
+								'listing_type',
+								'listing_status',
+								'listing_for_sale_or_for_lease',
+								'listing_property_name',
+								'listing_address',
+								'listing_city',
+								'listing_state',
+								'listing_zip',
+								'listing_neighborhood',
+								'listing_county',
+								'listing_year_built',
+								'listing_sub_type',
+								'listing_building_size',
+								'listing_lot_size',
+								'listing_apn_parcel_id',
+								'listing_number_of_units',
+								'listing_net_operating_income',
+								'listing_cap_rate',
+								'listing_description',
+								'listing_image_gallery',
+								'listing_unit_mix',
+								'listing_file_upload'
+							),
+							'submit_value' => 'Save Changes'
+						) );
+						?>
                     </div>
-					<?php acf_form( array(
-						'post_id'      => $post_id,
-						'post_title'   => false,
-						'new_post'     => array(
-							'post_type'   => 'mp-listing',
-							'post_status' => 'publish'
-						),
-						//'uploader'     => 'basic',
-						'fields'       => array(
-							'listing_mls_number',
-							'listing_price',
-							'listing_type',
-							'listing_status',
-							'listing_for_sale_or_for_lease',
-							'listing_property_name',
-							'listing_address',
-							'listing_city',
-							'listing_state',
-							'listing_zip',
-							'listing_neighborhood',
-							'listing_county',
-							'listing_year_built',
-							'listing_sub_type',
-							'listing_building_size',
-							'listing_lot_size',
-							'listing_apn_parcel_id',
-							'listing_number_of_units',
-							'listing_net_operating_income',
-							'listing_cap_rate',
-							'listing_description',
-							'listing_image_gallery',
-							'listing_unit_mix',
-							'listing_file_upload'
-						),
-						'submit_value' => 'Save Changes'
-					) );
-					?>
+					<?php get_template_part( 'template-parts/logged-in-user-sidebar' ); ?>
                 </div>
-	            <?php get_template_part( 'template-parts/logged-in-user-sidebar' ); ?>
             </div>
-        </div>
-    </main>
-</div>
+        </main>
+    </div>
 <?php
 get_footer();
