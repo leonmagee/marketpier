7b4e70165af74163d19f7ac31c716ca03d7b9b18
Leon Magee <leonmagee@hotmail.com>
2017-09-23 10:19:09 -0700

    udpate

--- inc/listing_data.php
+++ inc/listing_data.php
@@ -143,6 +143,7 @@ class listing_data {
 		}
 		//get the listing date as post date
 		$this->listing_date = get_the_date( 'n/j/Y' );
+		$this->last_updated = $this->listing_date;
 		$post_date          = get_the_date( 'U' );
 		//$post_date = get_the_date( 'U', true ); // for GMT
 		$current_date          = time();
