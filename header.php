48c6c0b29f47c017936f7b0f40b87b01f9828e2c
Leon Magee <leonmagee@hotmail.com>
2017-10-02 10:32:36 -0700

    update

--- header.php
+++ header.php
@@ -16,7 +16,10 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="shortcut icon" href="<?php echo get_field( 'favicon', 'option' ); ?>" type="image/x-icon"/>
+    <?php $favicon_url = get_field( 'favicon', 'option' );
+    var_dump( $favicon_url );
+    ?>
+    <link rel="shortcut icon" href="<?php echo $favicon_url ?>" type="image/x-icon"/>
 	<?php wp_head(); ?>
 </head>
 
