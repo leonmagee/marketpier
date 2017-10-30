13cd1e89b22f2ac4a5e3e0654a043489454653bc
Leon Magee <leonmagee@hotmail.com>
2017-10-29 18:14:54 -0700

    update

---
+++ page-homepage-template-compliance-ridx.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Template Name: Homepage Compliance rIDX
+ *
+ * @package MarketPier
+ */
+
+require_once( 'inc/form-process-submit.php' );
+
+get_header();
+
+if ( $background = get_field( 'homepage_background_image', 'option' ) ) {
+	$bg_style = "style='background-image: url(" . $background . ")'";
+} else {
+	$bg_style = '';
+}
+
+?>
+
+    <div class="homepage-wrap-outer">
+        <div class="homepage-slider" <?php echo $bg_style; ?>>
+            <div class="homepage-slider-overlay">
+                <div class="homepage-slider-inner">
+                    <div class="slider-items">
+						<?php if ( $main_tagline = get_field( 'main_tagline', 'option' ) ) { ?>
+                            <h1><?php echo $main_tagline; ?></h1>
+						<?php }
+						get_template_part( 'template-parts/homepage-form-compliance-ridx' ); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="homepage-section homepage-section-cta">
+
+            <div class="homepage-cta-boxes-outer">
+				<?php
+				$homepage_cta_boxes = get_field( 'homepage_cta_boxes', 'option' );
+				if ( $homepage_cta_boxes ) { ?>
+                    <div class="homepage-cta-boxes-wrap">
+						<?php foreach ( $homepage_cta_boxes as $cta_box ) { ?>
+                            <div class="homepage-cta-boxes-inner">
+                                <div class="cta-inside">
+                                    <div class="title-wrap">
+                                        <img src="<?php echo $cta_box['icon']; ?>"/>
+                                        <h3>
+                                            <a href="#">
+												<?php echo $cta_box['title']; ?>
+                                            </a>
+                                        </h3>
+                                    </div>
+                                    <p><?php echo $cta_box['excerpt']; ?></p>
+                                    <a class='cta-link'
+                                       href="#"><?php echo $cta_box['link_text']; ?></a>
+                                </div>
+                            </div>
+						<?php } ?>
+                    </div>
+				<?php } ?>
+            </div>
+
+        </div>
+
+    </div>
+
+
+<?php
+get_footer();
---
+++ template-parts/homepage-form-compliance-ridx.php
@@ -0,0 +1,46 @@
+<!-- @todo this should pull options from custom field repeater -->
+<div class="search-form-wrap">
+    <form method="post" action="#"><!-- @todo form action to switch page to search results? -->
+        <div class="main-form-inner">
+            <input type="hidden" name="listing-search-form"/>
+			<?php if ( $for_sale_lease_options = get_field( 'for_sale_for_lease_select_options', 'option' ) ) { ?>
+                <div class="input-wrap for-sale-for-lease" data-toggle="status-dropdown">
+                    <div class="select-toggle">For Sale</div>
+                    <i class="fa fa-sort" aria-hidden="true"></i>
+                    <div class="dropdown-pane" id="status-dropdown" data-dropdown data-hover="true"
+                         data-hover-pane="true">
+                        <ul>
+							<?php foreach ( $for_sale_lease_options as $option ) {
+								echo '<li>' . $option['status'] . '</li>';
+							} ?>
+                        </ul>
+                    </div>
+                    <input class="hidden-input" type="hidden" name="status"/>
+                </div>
+			<?php } ?>
+			<?php if ( $property_type_options = get_field( 'property_type_select_options', 'option' ) ) { ?>
+            <div class="input-wrap property-type" data-toggle="property-type-dropdown">
+                <div class="select-toggle">All Property Types</div>
+                <i class="fa fa-sort" aria-hidden="true"></i>
+                <div class="dropdown-pane" id="property-type-dropdown" data-dropdown data-hover="true"
+                     data-hover-pane="true">
+                    <ul>
+						<?php foreach ( $property_type_options as $option ) {
+							echo '<li>' . $option['property_type'] . '</li>';
+						} ?>
+                    </ul>
+                </div>
+				<?php } ?>
+                <input class="hidden-input" type="hidden" name="property-type"/>
+            </div>
+            <div class="input-wrap main-input">
+                <input type="text" placeholder="<?php echo get_field( 'search_input_placeholder', 'option' ); ?>"
+                       name="city-zip"/>
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </div>
+            <a href="https://google.com" class="compliance-a-tag">
+                <input class="submit-input compliance" value="SEARCH"/>
+            </a>
+        </div>
+    </form>
+</div>
