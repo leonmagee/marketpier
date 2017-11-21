dec15d0570ab608bf56aae2b1b60022bafe4e4d5
Leon Magee <leonmagee33@gmail.com>
2017-11-21 08:45:52 -0800

    udpate

--- single.php
+++ single.php
@@ -8,14 +8,13 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			xxxx
 			<div class="page-content-wrap">
 				<?php
 				while ( have_posts() ) : the_post();
 					get_template_part( 'template-parts/content', 'page' );
 				endwhile; // End of the loop.
-				the_post_navigation();
-										// If comments are open or we have at least one comment, load up the comment template.
+					the_post_navigation();
+						// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
