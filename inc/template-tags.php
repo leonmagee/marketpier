d128aa7aa02f38969b01f53854bc955767f0900a
Leon Magee <leonmagee33@gmail.com>
2017-11-22 09:26:48 -0800

    update

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -24,19 +24,26 @@ function marketpier_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
-		/* translators: %s: post date. */
-		esc_html_x( 'Posted on %s', 'post date', 'marketpier' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+	// $posted_on = sprintf(
+	// 	/* translators: %s: post date. */
+	// 	esc_html_x( 'Posted on %s', 'post date', 'marketpier' ),
+	// 	'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	// );
+	
+
+	// $byline = sprintf(
+	// 	/* translators: %s: post author. */
+	// 	esc_html_x( 'by %s', 'post author', 'marketpier' ),
+	// 	'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	// );
 
 	$byline = sprintf(
 		/* translators: %s: post author. */
 		esc_html_x( 'by %s', 'post author', 'marketpier' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on">' . $time_string . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
