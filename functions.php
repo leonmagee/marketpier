caf921ea6bc07a680021789094ba118a74f84446
Leon Magee <leonmagee33@gmail.com>
2017-12-18 20:35:15 -0800

    update

--- functions.php
+++ functions.php
@@ -185,7 +185,7 @@ function marketpier_scripts() {
 
 	wp_enqueue_style( 'foundation' );
 
-	wp_register_style( 'marketpier-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.38' );
+	wp_register_style( 'marketpier-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.39' );
 
 	wp_enqueue_style( 'marketpier-styles' );
 
