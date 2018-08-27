17e35eaacc9e54c2b14e490b829e09a5ab23db09
Leon Magee <leonmagee33@gmail.com>
2018-08-27 12:01:15 -0700

    update

--- page-landing-page-template.php
+++ page-landing-page-template.php
@@ -22,7 +22,7 @@ $header_image_url = get_field('header_image');
 
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                         <header class="entry-header">
-                            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                            <?php the_title( '<h1 class="landing-title">', '</h1>' ); ?>
                         </header><!-- .entry-header -->
 
                         <div class="entry-content">
